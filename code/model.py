26d643eeb6a526c11686aeda748995422107aefa
yoinkbird <YoinkBird2010@gmail.com>
2017-11-27 20:26:16 -0600

    refactor: moving roc-curve plot routine to function, part1b

--- code/model.py
+++ code/model.py
@@ -239,6 +239,7 @@ def dectree_evaluate_cv_strategy(X_full, y_full):
 # <def_plot_roc_curve>
 def plot_roc_curve(clf, data, predictors, responsecls):
     from sklearn.model_selection import StratifiedKFold,GroupKFold
+    my_graphics = options['graphics']
     #--------------------------------------------------------------------------------------------------------------
     # ROC curve
     # source: http://scikit-learn.org/stable/auto_examples/model_selection/plot_roc_crossval.html#sphx-glr-auto-examples-model-selection-plot-roc-crossval-py
@@ -267,26 +268,26 @@ def plot_roc_curve(clf, data, predictors, responsecls):
         tprs[-1][0] = 0.0
         roc_auc = auc(fpr, tpr)
         aucs.append(roc_auc)
-        if (options['graphics'] == 1):
+        if (my_graphics == 1):
             plt.plot( fpr, tpr, lw=1, alpha=0.3, label = 'ROC fold %d (AUC = %0.2f)' % (i, roc_auc))
         i += 1
-    if (options['graphics'] == 1):
+    if (my_graphics == 1):
         plt.plot( [0, 1], [0, 1], linestyle='--', lw=2, color='r', label='50%', alpha=0.8)
     # plot average true-positive-rate vs average false-positive-rate
     mean_tpr = np.mean(tprs, axis=0)
     mean_tpr[-1] = 1.0
     mean_auc = auc(mean_fpr, mean_tpr)
     std_auc = np.std(aucs)
-    if ( options['graphics'] == 1):
+    if ( my_graphics == 1):
         plt.plot(mean_fpr, mean_tpr, color='b', label=r'Mean ROC (AUC= %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=0.8)
     # plot average false-positive-rate vs
     std_tpr = np.std(tprs, axis=0)
     tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
     tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-    if (options['graphics'] == 1):
+    if (my_graphics == 1):
         plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=0.2, label=r'$\pm$ 1 std. dev.')
 
-    if (options['graphics'] == 1):
+    if (my_graphics == 1):
         # options
         plt.xlim([-0.05, 1.05])
         plt.ylim([-0.05, 1.05])
