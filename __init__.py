65dfc32b098c2a81e028968c14addaf08faeecfa
yoinkbird <YoinkBird2010@gmail.com>
2018-09-08 13:55:50 -0500

    refactor: module_import - enabling initial hacky import in conjunction with code in server repo

---
+++ __init__.py
@@ -0,0 +1,18 @@
+#from modelgen.code.helpers import *
+#from modelgen.code.feature_definitions import *
+#from modelgen.code.txdot_parse import *
+#---
+#from code.helpers import *
+#from code.feature_definitions import *
+#from code.txdot_parse import *
+#--- vv good vv
+from code import *
+# vvv suggestion from src: https://stackoverflow.com/questions/16480898/receiving-import-error-no-module-named-but-has-init-py
+#from os import sys
+#sys.path.append("code")
+# ^^^
+print("hi - modelgen")
+
+# also referred to pandas __init__.py src : # https://github.com/pandas-dev/pandas/blob/master/pandas/__init__.py
+# __init__.py src: https://chrisyeh96.github.io/2017/08/08/definitive-guide-python-imports.html#converting-a-folder-of-scripts-into-an-importable-package-of-modules
+# __init__.py src: https://stackoverflow.com/questions/2183205/importing-a-module-in-nested-packages
---
+++ code/__init__.py
@@ -0,0 +1,5 @@
+# from helpers import *
+# from feature_definitions import *
+# from txdot_parse import *
+print("hi - modelgen/code")
+
